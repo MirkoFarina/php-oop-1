f59869744fc22cfdd2d8349d40747ebaf1c83cca
MirkoFarina <MirkoFarina@outlook.com>
2022-12-07 15:47:30 +0100

    [add] new film

--- db.php
+++ db.php
@@ -9,6 +9,13 @@ $film_1->getPoster('https://pad.mymovies.it/filmclub/2022/08/122/locandina.jpg')
 
 $film_2= new Movie('Troll', 'Nv', new Genres('Norvegese', 'Film d\'azione'));
 
+
 $films = [$film_1, $film_2];
+
+$film_3 = new Movie('Enola Holmes 2', 'En', new Genres('Drammi a sfondo sociale', 'Film per famiglie', 'Serie e Film storici'), 'Ora detective a tutti gli effetti come il frattelo maggiore scherlock ....');
+$film_3->rating('3');
+$film_3->getPoster('https://www.mbmusic.it/wp-content/uploads/2022/11/Enola-Holmes-2-Colonna-Sonora-Film-2022-Netflix.jpg');
+
+$films[] = $film_3;
 
 
